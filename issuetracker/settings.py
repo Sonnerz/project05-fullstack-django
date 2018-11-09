7a487e9cca6d768d34f294454755ae61df48fc1c
Sonnerz <Sonnerz@users.noreply.github.com>
2018-11-09 12:05:55 +0000

    Added location for media to AWS

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -167,7 +167,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
 MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
