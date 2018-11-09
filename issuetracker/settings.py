7b8defcdc56f3104ae35e16d37e0b30fd9bb58da
Sonnerz <Sonnerz@users.noreply.github.com>
2018-11-09 10:56:38 +0000

    Set up site for AWS  - Amazon web services Added AWS static folder and media folder This git push is to test that everyting works on AWS and Heroku

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'checkout',
     'cart',
     'search',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -144,8 +145,27 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000',
+}
+
+AWS_STORAGE_BUCKET_NAME = 'ci-issuetracker'
+AWS_S3_REGION_NAME = 'eu-west-1'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
