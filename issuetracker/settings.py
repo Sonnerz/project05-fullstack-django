67e3b5d94d2000610c1cd9fdcb9ace41c5546304
Sonnerz <Sonnerz@users.noreply.github.com>
2018-11-14 18:56:11 +0000

    Styling Login page Styling password reset pages

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -177,7 +177,8 @@ STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
